eabfac130b591ad206f215e0eed80ce054dc76ca
UhuruV <uhuruvalary@gmail.com>
2021-02-11 23:35:35 +0300

    custom query pagination

--- header.php
+++ header.php
@@ -25,7 +25,7 @@ wp_head();
               ><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li <?php 
-              if (get_post_type() == 'event') {
+              if (get_post_type() == 'event' OR is_page('past-events')) {
                 echo 'class = "current-menu-item"';
               } 
               ?>><a href="<?php echo get_post_type_archive_link('event') ?>">Events</a></li>
